69163eb3a939451a4d45512029532b107d2b3ce4
VamsiKrishnaRachaamdugu <r.v.k.0002@gmail.com>
2019-09-16 14:34:41 +0530

    assessment set added

--- assessment-1/eval_strings.py
+++ assessment-1/eval_strings.py
@@ -1,14 +1,16 @@
-ip_list=['five plus three','seven minus two','two plus eight minus five','eight divide four']
-ref_dict={'plus':'+','minus':'-','divide':'/','two':'2','three':'3','four':'4','five':'5','seven':'7','eight':'8'}
-p_list=[]
+ip_list = ['five plus three', 'seven minus two', 'two plus eight minus five', 'eight divide four']
+ref_dict = {'plus': '+', 'minus': '-', 'divide': '/', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'seven': '7',
+            'eight': '8'}
+p_list = []
 for i in ip_list:
-    str_list=i.split(' ')
-    string=''
-    for i in str_list:
-        string=string+ref_dict[i]
+    str_list = i.split(' ')
+    string = ''
+    for j in str_list:
+        string = string + ref_dict[j]
     p_list.append(string)
-op_list=[]
+op_list = []
 for i in p_list:
-    op_list.append(eval(i))
+    for j, k in ref_dict.items():
+        if str(int(eval(i))) == k:
+            op_list.append(j)
 print(op_list)
-
